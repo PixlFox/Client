8df14218f5aa4483a7a2752eb64d72725143c623
Ashton Storks <ashtonj7289@gmail.com>
2017-05-18 01:36:36 -0600

    More ui tweaks (Clickable item animations)

--- app/components/community.component.ts
+++ app/components/community.component.ts
@@ -15,7 +15,7 @@ export class CommunityComponent implements AfterViewInit, OnDestroy {
 
 	ngAfterViewInit() {
 		this.webView = document.querySelector('#community-webview');
-		this.webView.openDevTools();
+		//this.webView.openDevTools();
 	}
 
 	ngOnDestroy(): void {
